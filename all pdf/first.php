6fd8b3ad86cb97878088b8b0426b8edd076c1396
VOID!0 <umarh.00234@gmail.com>
2024-12-05 23:04:24 +0100

    s

---
+++ all pdf/first.php
@@ -0,0 +1,6 @@
+<?php
+echo "hello there.so i hear you'are learning to be a php programmer!\n";
+echo "why don't you type in your name for me:\n";
+$name=trim(fgets(STDIN));
+echo "\n thanks," .$name .",it's really nice to meet you .\n\n";
+?>
